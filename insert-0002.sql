6a99fde5af8a08447caadc85e9129becdcd0b742
wagnerelvio <wagnerelvio@gmail.com>
2025-03-16 13:18:25 -0300

    Create insert-0002.sql

---
+++ insert-0002.sql
@@ -0,0 +1,4 @@
+INSERT INTO pessoas (FirstName, LastName, Idade, Cidade, Estado, Pais)  
+VALUES  
+('Ana', 'Santos', 25, 'Rio de Janeiro', 'RJ', 'Brasil'),  
+('João', 'Pereira', 40, 'Curitiba', 'PR', 'Brasil');
