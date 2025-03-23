4d2bfb94db362a4630cd60904785030f298d4106
wagnerelvio <wagnerelvio@gmail.com>
2025-03-23 17:35:42 -0300

    Create Table_Pedidos.sql

---
+++ Table_Pedidos.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Pedido (
+    id_pedido INT AUTO_INCREMENT PRIMARY KEY,
+    id_cliente INT NOT NULL,
+    valor DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
