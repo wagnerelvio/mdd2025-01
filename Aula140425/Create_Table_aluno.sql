fa66a3910d0093465ac792df21cc59f615193f6a
wagnerelvio <wagnerelvio@gmail.com>
2025-04-13 12:59:40 -0300

    Create Create_Table_aluno.sql

---
+++ Aula140425/Create_Table_aluno.sql
@@ -0,0 +1,8 @@
+-- Criar a tabela aluno
+ 
+CREATE TABLE aluno (
+    idaluno INTEGER PRIMARY KEY,
+    nomealuno TEXT NOT NULL,
+    idcurso INTEGER,
+    FOREIGN KEY (idcurso) REFERENCES curso(idcurso)
+);
