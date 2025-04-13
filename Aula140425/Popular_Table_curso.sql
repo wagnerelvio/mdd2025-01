b3b727ca071a7f985b4bdaad0359e9e966840310
wagnerelvio <wagnerelvio@gmail.com>
2025-04-13 13:00:15 -0300

    Create Popular_Table_curso.sql

---
+++ Aula140425/Popular_Table_curso.sql
@@ -0,0 +1,7 @@
+INSERT INTO curso (idcurso, nomecurso) VALUES
+(1, 'Engenharia de Software'),
+(2, 'Sistemas de Informação'),
+(3, 'Ciência da Computação'),
+(4, 'Engenharia Civil'),
+(5, 'Arquitetura e Urbanismo'),
+(6, 'Engenharia Elétrica');
