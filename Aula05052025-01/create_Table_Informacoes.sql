35e45caa4ed77c8e06457e5c5dc88fc475f2c2b4
wagnerelvio <wagnerelvio@gmail.com>
2025-05-04 13:38:00 -0300

    Create create_Table_Informacoes.sql

---
+++ Aula05052025-01/create_Table_Informacoes.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Informacoes (
+    Id SERIAL PRIMARY KEY,
+    NomeCompleto VARCHAR(100) NOT NULL,
+    Curso VARCHAR(100) NOT NULL,
+    Disciplina VARCHAR(100) NOT NULL,
+    Idade INTEGER
+);
